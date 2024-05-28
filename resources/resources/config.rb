9b7b3b25a7c64a082332687cc4d7c0702267c59b
Miguel Negron <manegron@redborder.com>
2024-05-29 00:04:42 +0100

    lint resources

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -1,22 +1,20 @@
-# Cookbook Name:: Consul
-#
+# Cookbook:: Consul
 # Resource:: config
-#
 
 actions :add, :remove, :install
 default_action :add
 
-attribute :user, :kind_of => String, :default => "root"
-attribute :group, :kind_of => String, :default => "root"
+attribute :user, kind_of: String, default: 'root'
+attribute :group, kind_of: String, default: 'root'
 
-attribute :confdir, :kind_of => String, :default => "/etc/consul"
-attribute :datadir, :kind_of => String, :default => "/tmp/consul"
+attribute :confdir, kind_of: String, default: '/etc/consul'
+attribute :datadir, kind_of: String, default: '/tmp/consul'
 
-attribute :ipaddress, :kind_of => String, :default => "0.0.0.0"
-attribute :cdomain, :kind_of => String, :default => "redborder.cluster"
+attribute :ipaddress, kind_of: String, default: '0.0.0.0'
+attribute :cdomain, kind_of: String, default: 'redborder.cluster'
 
-attribute :dns_local_ip, :kind_of => Array
+attribute :dns_local_ip, kind_of: Array
 
-attribute :is_server, :kind_of => [TrueClass, FalseClass]
+attribute :is_server, kind_of: [TrueClass, FalseClass]
 
-attribute :log_level, :kind_of => String, :default => "INFO"
+attribute :log_level, kind_of: String, default: 'INFO'
