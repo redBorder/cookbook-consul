fc96a00e7c380e340a1ac1c58250cdfd14de4e08
jsotofernandez <127120525+jsotofernandez@users.noreply.github.com>
2024-05-29 13:53:44 +0100

    Revert "Improvement/fix lint "

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -1,6 +1,7 @@
 name             'consul'
-maintainer       'Eneo Tecnología S.L.'
-maintainer_email 'git@redborder.com'
-license          'AGPL-3.0'
+maintainer       'redborder'
+maintainer_email 'cjmateos@redborder.com'
+license          'All rights reserved'
 description      'Installs/Configures zookeeper'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.7'
--- resources/recipes/default.rb
+++ resources/recipes/default.rb
@@ -1,21 +1,27 @@
-# Cookbook:: consul
+#
+# Cookbook Name:: consul
 # Recipe:: default
-# Copyright:: 2024, redborder
-# License:: Affero General Public License, Version 3
+#
+# Redborder, 2016
+#
+# AFFERO GENERAL PUBLIC LICENSE V3
+#
 
-consul_config 'Configure Consul' do
-  confdir node['consul']['confdir']
-  datadir node['consul']['datadir']
-  ipaddress node['ipaddress']
-  cdomain node['redborder']['cdomain']
-  dns_local_ip node['consul']['dns_local_ip']
+# For testing purposes
+
+consul_config "Configure Consul" do
+  confdir node["consul"]["confdir"]
+  datadir node["consul"]["datadir"]
+  ipaddress node["ipaddress"]
+  cdomain node["redborder"]["cdomain"]
+  dns_local_ip node["consul"]["dns_local_ip"]
   action :add
 end
 
-# consul_config 'Configure Consul' do
-#   confdir node['consul']['confdir']
-#   datadir node['consul']['datadir']
-#   cdomain node['redborder']['cdomain']
-#   dns_local_ip node['consul']['dns_local_ip']
-#   action :remove
-# end
+#consul_config "Configure Consul" do
+#  confdir node["consul"]["confdir"]
+#  datadir node["consul"]["datadir"]
+#  cdomain node["redborder"]["cdomain"]
+#  dns_local_ip node["consul"]["dns_local_ip"]
+#  action :remove
+#end
