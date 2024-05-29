9147314e8993f398f7031adc0d9daac292a0cb83
jsotofernandez <127120525+jsotofernandez@users.noreply.github.com>
2024-05-29 12:59:01 +0100

    Revert "Improvement/fix lint"

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -1,7 +1,7 @@
-default['consul']['user'] = 'root'
-default['consul']['group'] = 'root'
-default['consul']['confdir'] = '/etc/consul'
-default['consul']['datadir'] = '/tmp/consul'
-default['consul']['dns_local_ip'] = []
-default['consul']['configured'] = false
-default['consul']['is_server'] = nil
+default["consul"]["user"] = "root"
+default["consul"]["group"] = "root"
+default["consul"]["confdir"] = "/etc/consul"
+default["consul"]["datadir"] = "/tmp/consul"
+default["consul"]["dns_local_ip"] = []
+default["consul"]["configured"] = false
+default["consul"]["is_server"] = nil
